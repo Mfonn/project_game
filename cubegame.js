a356cfb553c91a1cbfdee657dbb9f4f469601238
Mfonn <67243684+Mfonn@users.noreply.github.com>
2020-08-08 10:35:51 +0100

    Update cubegame.js

--- cubegame.js
+++ cubegame.js
@@ -23,7 +23,7 @@ function startGame() {
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-      this.canvas.width = 30;
+      this.canvas.width = 320;
       this.canvas.height = 500;
       this.context = this.canvas.getContext("2d");
       document.body.insertBefore(this.canvas, document.body.childNodes[0]);
