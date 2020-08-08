87f7f26a7267a090a041bfe4e58355d8214c198d
Mfonn <67243684+Mfonn@users.noreply.github.com>
2020-08-08 11:06:37 +0100

    Update cubegame.js

--- cubegame.js
+++ cubegame.js
@@ -24,7 +24,7 @@ var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
       this.canvas.width = 320;
-      this.canvas.height = 500;
+      this.canvas.height = 400;
       this.context = this.canvas.getContext("2d");
       document.body.insertBefore(this.canvas, document.body.childNodes[0]);
       this.frameNo = 0; //for multiple obstacles
